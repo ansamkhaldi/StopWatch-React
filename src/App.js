62b5c09d02bf94291a483a65968facf47336f9a0
Ansam Khaldi] <[ansamkhaldi95@gmail.com]>
2020-11-11 23:42:27 +0200

    add stopwatch-react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Stopwatch from './Stopwatch';
+
+
+
+
+function App() {
+  return (
+    <div className="stopwatch">
+      <h1> STOP-WATCH</h1>
+       <Stopwatch/>
+    </div>
+  );
+}
+
+export default App;
