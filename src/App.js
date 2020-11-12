6ba5e5b876699baab0e5e3cdc131ade599794ad5
Ansam Khaldi] <[ansamkhaldi95@gmail.com]>
2020-11-12 15:44:39 +0200

    update the extension prettier and Eslint

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
-import Stopwatch from './Stopwatch';
-
-
-
+import Stopwatch from "./Stopwatch";
 
 function App() {
   return (
     <div className="stopwatch">
       <h1> STOP-WATCH</h1>
-       <Stopwatch/>
+      <Stopwatch />
     </div>
   );
 }
